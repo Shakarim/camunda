9dc05f0215683e3b646bed1b9e59ea3585cced19
Shakarim Sapa <info@relitech.kz>
2020-04-30 23:20:37 +0600

    Changes configs

--- config/config.exs
+++ config/config.exs
@@ -1,9 +1,3 @@
 use Mix.Config
 
-config :camunda,
-       :camunda,
-       hostname: "http://localhost:8080/engine-rest",
-       username: "demo",
-       password: "demo"
-
 import_config "#{Mix.env()}.exs"
---
+++ config/dev.exs
@@ -0,0 +1,5 @@
+config :camunda,
+       :camunda,
+       hostname: "http://localhost:8080/engine-rest",
+       username: "demo",
+       password: "demo"
