6764de9bdd3cdacef3d8098911f5c7f1ca3b2082
Shakarim Sapa <info@relitech.kz>
2020-04-30 23:30:27 +0600

    Adds Camunda.get_basic_auth_data/1 for getting base auth user login and pass from %Plug.Conn{}

--- lib/camunda.ex
+++ lib/camunda.ex
@@ -2,4 +2,31 @@ defmodule Camunda do
   @moduledoc ~S"""
   Documentation for Camunda.
   """
+
+  @doc """
+  Returns Basic Auth data from conn
+
+  ## Returns
+
+    [username, password]
+
+    [nil, nil]
+
+  ## Params
+
+    %Plug.Conn{}
+
+  """
+  def get_basic_auth_data(conn) do
+    with [token] <- Plug.Conn.get_req_header(conn, "authorization"),
+         [username, password] <- token
+                                 |> String.split(" ")
+                                 |> List.last()
+                                 |> Base.decode64!()
+                                 |> String.split(":") do
+      {username, password}
+    else
+      _ -> [nil, nil]
+    end
+  end
 end
