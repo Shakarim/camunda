46af715d1f6506f5130a4e96653a2dcd686e1fc3
Shakarim Sapa <info@relitech.kz>
2020-05-23 19:05:55 +0600

    Adds function for getting groups ids list

--- lib/camunda/group.ex
+++ lib/camunda/group.ex
@@ -9,7 +9,6 @@ defmodule Camunda.Group do
   Returns list of groups
   """
   def list(username, password, options \\ [])
-
   def list(username, password, options) do
     with request_headers <- ApiInstance.get_basic_header(username, password),
          request_url <- @list,
@@ -21,6 +20,23 @@ defmodule Camunda.Group do
       {status, result} -> {status, result}
       error -> {:error, error}
       _ -> {:error, "Unknown error of Camunda.Group.list/3"}
+    end
+  end
+
+  @doc ~S"""
+  Returns list of group ids
+  """
+  def get_group_ids(username, password, options \\ [])
+  def get_group_ids(username, password, options) do
+    with {:ok, groups} <- list(username, password, options) do
+      {
+        :ok,
+        groups
+        |> Enum.map(&(Map.get(&1, "id")))
+        |> Enum.filter(&(&1 !== nil))
+      }
+    else
+      error -> error
     end
   end
 end
