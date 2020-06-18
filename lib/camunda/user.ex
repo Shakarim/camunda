ac0806911f8a659aed357ad7c3beed98f590b9e4
Shakarim Sapa <info@relitech.kz>
2020-06-18 22:12:44 +0600

    Adds Camunda.User module for app

---
+++ lib/camunda/user.ex
@@ -0,0 +1,27 @@
+defmodule Camunda.User do
+  @moduledoc :false
+
+  @profile "/user/{id}/profile"
+
+  @doc ~S"""
+  Returns user profile by user identity
+
+  ## Examples
+
+    iex> Camunda.User.get_user_profile_by_id("demo", "demo", "demo")
+
+  """
+  def get_user_profile_by_id(id, username, password, options \\ []) do
+    with req_headers <- ApiInstance.get_basic_header(username, password),
+         req_url <- String.replace(@profile, "{id}", id),
+         {:ok, %HTTPoison.Response{} = response} <- ApiInstance.get(req_url, req_headers, options),
+         {:ok, result} <- ApiInstance.get_request_result(response)
+      do
+      {:ok, result}
+    else
+      {status, result} -> {status, result}
+      error -> {:error, error}
+      _ -> {:error, "Unknown error of Camunda.User.get_user_by_id/4"}
+    end
+  end
+end
