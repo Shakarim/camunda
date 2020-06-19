9e9c2eb7234e606da599954516c08f1e08ae0509
Shakarim Sapa <info@relitech.kz>
2020-06-19 14:53:54 +0600

    Adds Camunda.Group.get_by_id/3 function

--- lib/camunda/group.ex
+++ lib/camunda/group.ex
@@ -4,6 +4,7 @@ defmodule Camunda.Group do
   """
 
   @list "/group"
+  @get "/group/{id}"
 
   @doc ~S"""
   Returns list of groups
@@ -20,6 +21,38 @@ defmodule Camunda.Group do
       {status, result} -> {status, result}
       error -> {:error, error}
       _ -> {:error, "Unknown error of Camunda.Group.list/3"}
+    end
+  end
+
+
+  @doc ~S"""
+  Returns group by identity
+
+  ## Examples
+
+    iex> Camunda.Group.get_by_id "abfsmExpertBySolidMining", "demo", "demo"
+    {
+      :ok,
+      %{
+        "id" => "abfsmExpertBySolidMining",
+        "name" => "╨г╨Ю ╨┐╨╛ ╤В╨▓╨╡╤А╨┤╤Л╨╝ ╨┐╨╛╨╗╨╡╨╖╨╜╤Л╨╝ ╨╕╤Б╨║╨╛╨┐╨░╨╡╨╝╤Л╨╝. ╨н╨║╤Б╨┐╨╡╤А╤В ╨┐╨╛ ╨в╨Я╨Ш",
+        "type" => "abfsm"
+      }
+    }
+
+  """
+  def get_by_id(id, username, password, options \\ [])
+  def get_by_id(id, username, password, options) do
+    with req_headers <- ApiInstance.get_basic_header(username, password),
+         req_url <- String.replace(@get, "{id}", id),
+         {:ok, %HTTPoison.Response{} = response} <- ApiInstance.get(req_url, req_headers, options),
+         {:ok, result} <- ApiInstance.get_request_result(response)
+      do
+      {:ok, result}
+    else
+      {status, result} -> {status, result}
+      error -> {:error, error}
+      _ -> {:error, "Unknown error of Camunda.Group.get_by_id/3"}
     end
   end
 
